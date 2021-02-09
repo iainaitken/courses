b4183f280fd43b68212e88480f34fcd420d0fa6f
iainaitken <iain_aitken@yahoo.co.uk>
2021-02-09 06:11:39 +0000

    add notes for lesson 15

--- udemy/testing_ruby/assignments/spec/card_spec.rb
+++ udemy/testing_ruby/assignments/spec/card_spec.rb
@@ -6,9 +6,14 @@ class Card
   end
 end
 
-RSpec.describe 'Card' do
-  it 'has a type' do
-    card = Card.new('Ace of Spades')
-    expect(card.type).to eq('Ace of Spades')
+RSpec.describe Card do
+  it 'has a rank' do
+    card = Card.new('Ace', 'Spades')
+    expect(card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    card = Card.new('Ace', 'Spades')
+    expect(card.suit).to eq('Spades')
   end
 end
