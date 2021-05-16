df7f615b147cf6a03990b1584b4e0dae07420238
iainaitken <iain_aitken@yahoo.co.uk>
2021-05-16 10:30:01 +0100

    Add assignment for lesson 10

---
+++ udemy/complete_javascript/code/01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,11 @@
+/* Values and Variables */
+
+let country = 'UK';
+let continent = 'Europe';
+let population = '66.65 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/* Data Types */
